56d805bb4fa5bf7f5e9c44b5e95ce355c4ea3bc4
William Estoque <william.estoque@gmail.com>
2017-03-27 18:06:07 +0800

    Add initial JS testing/lint setup

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    jasmine : {
+      src : 'src/**/*.js',
+      options : {
+        specs : 'spec/**/*.js'
+      }
+    },
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'src/**/*.js',
+        'spec/**/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('test', ['jshint', 'jasmine']);
+
+  grunt.registerTask('default', ['test']);
+};
